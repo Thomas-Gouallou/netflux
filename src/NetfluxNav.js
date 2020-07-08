1bbd9b45c1fc54007c0891c258e1708f5be4e5e3
Thomas Gouallou <thomas.gouallou@yahoo.fr>
2020-07-08 15:03:24 +0200

    ajout data et Nav

--- src/NetfluxNav.js
+++ src/NetfluxNav.js
@@ -1,5 +1,31 @@
-import { React, Component } from 'react';
+import React, { Component } from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Header from ""
 
-export class Netflux extends Component{
-    
+export class Navigation extends Component {
+
+    render() {
+        return(
+            <BrowserRouter>
+                <Header></Header>
+                <Switch>
+                    <Route path='/' exact>
+                        <Acceuil></Acceuil>
+                    </Route>
+                    <Route  >
+                        <DetailProgramme></DetailProgramme>
+                    </Route>
+                    <Route >
+                        <FormAjout ></FormAjout>
+                    </Route>
+                    <Route >
+                        <Search></Search>
+                    </Route><Route >
+                        <Favoris></Favoris>
+                    </Route>
+                </Switch>
+                <Footer></Footer>
+            </BrowserRouter>
+        )
+    }
 }
