78354447a81d9a01cd95df36e2df553ad53bbd79
Thomas Gouallou <thomas.gouallou@yahoo.fr>
2020-07-08 16:51:44 +0200

    fin Header

--- src/netflux-app/Header.js
+++ src/netflux-app/Header.js
@@ -8,10 +8,11 @@ class Header extends Component {
         return (
             <header className="container-fluid">
                 <nav className="navbar navbar-expand-lg navbar-light bg-dark text-alert row">
-                    <ul className="navbar-nav mr-auto ">
-                        <li className="nav-item ">
+                    
+                    <a className="nav-item ">
                             <Link to='/' className="nav-link ="><img src={Netflux} alt="logo netflux"/></Link>
-                        </li>
+                    </a>
+                    <ul className="navbar-nav mr-auto col-6">
                         <li className="nav-item">
                             <Link to='/Series' className="nav-link text-danger">Séries</Link>
                         </li>
@@ -21,20 +22,20 @@ class Header extends Component {
                         <li className="nav-item">
                             <Link to= '/Nouveaute' className="nav-link text-danger">Nouveautés</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <Link to= '/MaListe' className="nav-link text-danger">Ma liste</Link>
                         </li>
                     </ul>
-                    <form class="form-inline my-2 my-lg-0">
+                    <form class="form-inline my-2 my-lg-0 col-2">
                         <input class="form-control mr-sm-2" type="search" placeholder="Rechercher"/>
                     </form>
-                    <span class="navbar-text text-danger">
+                    <span class="navbar-text text-danger ">
                         <Link to= '/Search' className="nav-link text-danger">Rechercher</Link>
                     </span>
-                    <span class="navbar-text text-danger">
+                    <span class="navbar-text text-danger col">
                         <Link to= '/Form' className="nav-link text-danger">Ajouter un film/une série</Link>
                     </span>
-                    <span class="navbar-text text-danger">
+                    <span class="navbar-text text-danger col">
                         <Link to= '/Compte' className="nav-link text-danger">Mon Compte</Link>
                     </span>
                 </nav>
